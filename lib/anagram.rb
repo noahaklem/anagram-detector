9753e24ff316c5ae2c26abe0168a75f6d92f74c9
Noah A Klem <noahaklem@gmail.com>
2021-09-01 10:18:05 -0600

    commit tree 5d68bdb96239b74e5e8791ec55a57b4cab9623f9

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select {|array_word| array_word.split("").sort == word.split("").sort}
+    end
+
+end
