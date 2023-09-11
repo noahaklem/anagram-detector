d54f581d98f8ae5aedc0fb9243d23adaa4fe0a93
Noah A Klem <noahaklem@gmail.com>
2023-09-10 20:11:43 -0600

    anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :name
+
+    def initialize(word)
+        @name = word.split("").sort
+    end
+
+    def match(array)
+        array.select do |word|
+            word.split("").sort == @name
+        end
+    end
+
+end
